d0c9a21672264ae9506686de9ff928e6ada96f15
Nathan Brooks <nathanbrooks@picknik.ai>
2021-09-02 21:31:12 -0600

    Changes for Galactic/Rolling

--- include/iirob_filters/gravity_compensation.h
+++ include/iirob_filters/gravity_compensation.h
@@ -46,7 +46,8 @@
 #include <boost/scoped_ptr.hpp>
 #include <geometry_msgs/msg/wrench_stamped.hpp>
 #include <geometry_msgs/msg/vector3_stamped.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2/LinearMath/Transform.h>
 #include <filters/filter_base.hpp>
